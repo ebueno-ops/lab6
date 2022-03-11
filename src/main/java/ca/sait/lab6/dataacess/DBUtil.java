0793e8c51ddb20bbee7ebda0105efd72455b18b9
Emmanuel Hombrebueno <emmanuel.hombrebueno@edu.sait.ca>
2022-03-11 11:33:00 -0700

    update

---
+++ src/main/java/ca/sait/lab6/dataacess/DBUtil.java
@@ -0,0 +1,25 @@
+package ca.sait.lab6.dataacess;
+
+import java.sql.*;
+
+public class DBUtil {
+    public static void closePreparedStatement(Statement ps) {
+        try {
+            if (ps != null) {
+                ps.close();
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+}
